8561f35e04fc20cc935126d251d0e541fdb54b33
Sittiphol Yooyod <20336055+yooyod@users.noreply.github.com>
2021-02-03 13:59:59 +0700

    Update pxt.json, main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+IR_RemoteRx.connectIrReceiver(DigitalPin.P8)
+IR_RemoteRx.onIrButton(IrButton.Num5, IrButtonAction.Pressed, function () {
+    
+    basic.showNumber(5)
+})
